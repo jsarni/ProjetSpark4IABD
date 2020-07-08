d0ca85e1700ea8558a6eee8b81856eeffb3b1ee1
jsarni <juba.sarni@gmail.com>
2020-07-08 12:03:56 +0200

    restructurer data processor

--- DataProcessor/src/main/scala/poc/prestacop/DataProcessor/DataProcessor.scala
+++ DataProcessor/src/main/scala/poc/prestacop/DataProcessor/DataProcessor.scala
@@ -1,16 +1,14 @@
 package poc.prestacop.DataProcessor
 
-import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
-import poc.prestacop.Commons.{AppConfig, schema}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import poc.prestacop.Commons.AppConfig
 import poc.prestacop.Commons.utils.HdfsUtils._
-import org.apache.spark.sql.functions._
 import org.apache.spark.storage.StorageLevel
 import poc.prestacop.Commons.schema.DroneViolationMessage
+import poc.prestacop.DataProcessor.AnalysisProcessor.FirstAnalysisProcessor
 
-import scala.util.Try
 class DataProcessor(dataFrame: DataFrame)(implicit sparkSession: SparkSession) {
 
-    import sparkSession.implicits._
     import DataProcessor._
 
     /**
@@ -31,48 +29,10 @@ class DataProcessor(dataFrame: DataFrame)(implicit sparkSession: SparkSession) {
 
     def run(): Unit = {
         for {
-            _ <- manageFirstAnalysis
+            _ <- FirstAnalysisProcessor(preparedDF).run
             _ = preparedDF.unpersist()
         } yield ()
     }
-
-    def manageFirstAnalysis: Try[Unit] = {
-        for {
-            winterDF <- winterViolationsDF
-            summerDF <- summerViolationsDF
-            unionDF = winterDF.union(summerDF)
-            aggregatedDF <- aggregationForFirstAnalysis(unionDF)
-            repartitionnedDF = aggregatedDF.repartition(1)
-            _ = writeToHdfs(repartitionnedDF, FIRST_ANALYSIS_TARGET_PATH, FIRST_ANALYSIS_TARGET_FORMAT, SaveMode.Overwrite)
-        } yield ()
-    }
-
-    def aggregationForFirstAnalysis(df: DataFrame): Try[DataFrame] = {
-        Try {
-            df
-              .groupBy("season")
-              .agg(count("violation_code").alias("number_of_violations"))
-        }
-    }
-
-    lazy val winterViolationsDF: Try[DataFrame] = {
-        Try{
-            preparedDF
-              .where(
-                  (date_format('sending_date, "MMdd") >= "1221" && date_format('sending_date, "MMdd") <= "1231") ||
-                    (date_format('sending_date, "MMdd") >= "0101" && date_format('sending_date, "MMdd") < "0321")
-            ).withColumn("season", lit("winter"))
-        }
-    }
-
-    lazy val summerViolationsDF: Try[DataFrame] = {
-        Try{
-            preparedDF.where(
-                date_format('sending_date, "MMdd") >= "0621" && date_format('sending_date, "MMdd") < "0921"
-            ).withColumn("season", lit("summer"))
-        }
-    }
-
 
 }
 
@@ -82,13 +42,11 @@ object DataProcessor extends AppConfig{
     val SPARK_DEFAULT_PARTITIONS: Int = conf.getInt("spark.default_partitions")
 
 
-    val FIRST_ANALYSIS_TARGET_PATH: String = conf.getString("hdfs.target.first_analysis_path")
+
     val SECOND_ANALYSIS_TARGET_PATH: String = conf.getString("hdfs.target.second_analysis_path")
     val THIRD_ANALYSIS_TARGET_PATH: String = conf.getString("hdfs.target.third_analysis_path")
     val FOURTH_ANALYSIS_TARGET_PATH: String = conf.getString("hdfs.target.fourth_analysis_path")
 
-
-    val FIRST_ANALYSIS_TARGET_FORMAT: String = conf.getString("hdfs.target.first_analysis_format")
     val SECOND_ANALYSIS_TARGET_FORMAT: String = conf.getString("hdfs.target.second_analysis_format")
     val THIRD_ANALYSIS_TARGET_FORMAT: String = conf.getString("hdfs.target.third_analysis_format")
     val FOURTH_ANALYSIS_TARGET_FORMAT: String = conf.getString("hdfs.target.fourth_analysis_format")
